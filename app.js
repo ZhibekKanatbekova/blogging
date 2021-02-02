4632337dec2cbb241390cb513192947b59ca2fe1
Zhibek <jiko633@gmail.com>
2021-02-02 22:11:17 +0400

    making pages connection

--- app.js
+++ app.js
@@ -16,13 +16,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-
-
   res.render("home", {la: homeStartingContent})
 })
 
+app.get("/about", function (req, res) {
+  res.render("about", { about: aboutContent });
+});
 
+app.get("/contact", function (req, res) {
+  res.render("contact", { contact: contactContent });
+});
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
 
 
